1156da23d6fab96a8bef7037f5e715793d82360f
Dewi Puspita Sari <dewibelajar10@gmail.com>
2025-03-23 15:51:35 +0700

    Menambahkan halaman AddTaskScreen

---
+++ app/src/main/java/com/dewipuspitasari0020/tasklist/ui/screen/AddTaskScreen.kt
@@ -0,0 +1,213 @@
+package com.dewipuspitasari0020.tasklist.ui.screen
+
+import android.app.DatePickerDialog
+import android.content.Context
+import android.content.res.Configuration
+import android.widget.DatePicker
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.outlined.DateRange
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.dewipuspitasari0020.tasklist.R
+import com.dewipuspitasari0020.tasklist.ui.theme.TasklistTheme
+import java.util.Calendar
+import java.util.Date
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddTaskScreen() {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(text = stringResource(R.string.add_task), textAlign = TextAlign.Center)
+                },
+                colors = TopAppBarDefaults.largeTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.inverseOnSurface,
+                    titleContentColor = MaterialTheme.colorScheme.inverseSurface,
+                ),
+                navigationIcon = {
+                    IconButton(
+                        onClick = {},
+                        colors = IconButtonDefaults.iconButtonColors(
+                            contentColor = MaterialTheme.colorScheme.inverseSurface
+                        )
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = stringResource(R.string.back)
+                        )
+                    }
+                },
+
+                )
+        }
+    ) { innerPadding ->
+        AddTask(modifier = Modifier.padding(innerPadding))
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddTask(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    var text by remember { mutableStateOf("") }
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .background(MaterialTheme.colorScheme.inverseOnSurface)
+            .padding(18.dp),
+        verticalArrangement = Arrangement.spacedBy(18.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.task_title),
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 20.sp,
+            color = MaterialTheme.colorScheme.primary,
+        )
+        TextField(
+            value = text,
+            onValueChange = { newText -> text = newText },
+            modifier = Modifier.fillMaxWidth(),
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = MaterialTheme.colorScheme.inversePrimary,
+                focusedIndicatorColor = Color.Blue,
+                unfocusedIndicatorColor = Color.Transparent
+            )
+        )
+        Text(
+            text = stringResource(R.string.task_description),
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 20.sp,
+            color = MaterialTheme.colorScheme.primary,
+        )
+        TextField(
+            value = text,
+            onValueChange = { newText -> text = newText },
+            modifier = Modifier.fillMaxWidth().height(150.dp),
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = MaterialTheme.colorScheme.inversePrimary,
+                focusedIndicatorColor = Color.Blue,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
+            maxLines = 5
+        )
+        DatePickerExample(context = context)
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(55.dp),
+            shape = RectangleShape,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.onPrimary),
+        ) {
+            Text(text = stringResource(R.string.create_new_task))
+        }
+    }
+}
+
+@Composable
+fun DatePickerExample(context: Context) {
+    val year: Int
+    val month: Int
+    val day: Int
+
+    val calendar = Calendar.getInstance()
+    year = calendar.get(Calendar.YEAR)
+    month = calendar.get(Calendar.MONTH)
+    day = calendar.get(Calendar.DAY_OF_MONTH)
+    calendar.time = Date()
+
+    val date = remember { mutableStateOf("") }
+
+    val datePickerDialog = DatePickerDialog(
+        context,
+        { _: DatePicker, selectedYear: Int, selectedMonth: Int, dayOfMonth: Int ->
+            date.value = "$dayOfMonth/${selectedMonth + 1}/$selectedYear"
+        }, year, month, day
+    )
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column {
+            Box(
+                modifier = Modifier
+                    .background(color = MaterialTheme.colorScheme.primary)
+                    .padding(8.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.DateRange,
+                    contentDescription = stringResource(R.string.add_task),
+                    tint = MaterialTheme.colorScheme.onPrimary
+                )
+            }
+        }
+        Column {
+            Button(
+                onClick = { datePickerDialog.show() },
+                shape = RectangleShape,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.inversePrimary,
+                    contentColor = MaterialTheme.colorScheme.primary
+                )
+            ) {
+                Text("Pilih Tanggal")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+private fun PrevHalamanTambah() {
+    TasklistTheme {
+        AddTaskScreen()
+    }
+}
